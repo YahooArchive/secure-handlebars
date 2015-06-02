855b9529d800d6b69ad76754ad7e65c01e60d875
Nera Liu <neraliu@gmail.com>
2015-06-02 19:19:12 +0800

    remove Object.create from polyfill.js of html-decoder

--- src/html-decoder/polyfills/polyfill.js
+++ src/html-decoder/polyfills/polyfill.js
@@ -1,21 +1,3 @@
-if (typeof Object.create != 'function') {
-  Object.create = (function() {
-    var Temp = function() {};
-    return function (prototype) {
-      if (arguments.length > 1) {
-        throw Error('Second argument not supported');
-      }
-      if (typeof prototype != 'object') {
-        throw TypeError('Argument must be an object');
-      }
-      Temp.prototype = prototype;
-      var result = new Temp();
-      Temp.prototype = null;
-      return result;
-    };
-  })();
-}
-
 /*! http://mths.be/codepointat v0.1.0 by @mathias */
 if (!String.prototype.codePointAt) {
   (function() {
